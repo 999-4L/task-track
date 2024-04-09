843411407c383dd12c262863f4fddcf98f5e4e0c
999-4L <112986907+999-4L@users.noreply.github.com>
2024-04-09 22:56:28 +0530

    Update FilterDel.jsx

--- src/components/FilterDel.jsx
+++ src/components/FilterDel.jsx
@@ -2,10 +2,10 @@ import FilterDropDown from "./FilterDropDown";
 
 function FilterDel({ handleFilter, handleDeleteAll }) {
   return (
-    <div className="filter my-5 flex flex-col xl:flex-row gap-3 justify-between items-center">
+    <div className="filter my-5 flex flex-row gap-3 justify-between items-center">
       <FilterDropDown handleFilter={handleFilter} />
       <button
-        className="bg-[#c61919] hover:bg-[#f74343] p-3 py-1 text-white rounded-md font-medium ml-1 h-[48px] max-w-[100px]"
+        className="bg-[#c61919] hover:bg-[#f74343] p-3 py-1 text-white rounded-md font-medium ml-1 h-[40px] max-w-[100px]"
         onClick={handleDeleteAll}
       >
         Delete All
