0c6aeb0a86046b2364ff1360547593f55d72f93b
999-4L <112986907+999-4L@users.noreply.github.com>
2024-04-09 22:55:34 +0530

    Update TaskItem.jsx

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -1,32 +1,32 @@
 function TaskItem({ task, handleEdit, handleDone, handleDelete }) {
   return (
-    <tr className="bg-gray-50 dark:bg-gray-800 border-b dark:border-gray-700 text-base">
+    <tr className="bg-gray-50 dark:bg-gray-800 border-b dark:border-gray-700 text-base flex flex-col lg:table-row rounded-xl mb-4">
       <th
         scope="row"
-        className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white text-center"
+        className="px-1 sm:px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white text-center text-wrap"
       >
         {task.name}
       </th>
-      <td className="px-6 py-4 text-center">{task.dueDate}</td>
-      <td className="px-6 py-4 text-center">
+      <td className="px-1 sm:px-6 py-4 text-center">{task.dueDate}</td>
+      <td className="px-1 sm:px-6 py-4 text-center">
         {task.isCompleted ? "Completed" : "Pending"}
       </td>
-      <td className="px-6 py-4">
+      <td className="px-1 sm:px-6 py-4">
         <div className="flex justify-center">
           <button
-            className="bg-amber-500 hover:bg-amber-400 p-3 py-1 text-white rounded-md font-medium mr-2"
+            className="bg-amber-500 hover:bg-amber-400 p-3 py-1 text-white rounded-md font-medium h-[40px] mr-2"
             onClick={() => handleEdit(task.name)}
           >
             Edit
           </button>
           <button
-            className="bg-[#c61919] hover:bg-[#f74343] p-3 py-1 text-white rounded-md font-medium mr-2"
+            className="bg-[#c61919] hover:bg-[#f74343] p-3 py-1 text-white rounded-md font-medium h-[40px] mr-2"
             onClick={() => handleDelete(task.name)}
           >
             Delete
           </button>
           <button
-            className="bg-emerald-700 hover:bg-emerald-600 p-3 py-1 text-white rounded-md font-medium"
+            className="bg-emerald-700 hover:bg-emerald-600 p-3 py-1 text-white rounded-md font-medium h-[40px] text-nowrap"
             onClick={() => handleDone(task.name)}
           >
             {task.isCompleted ? "Mark As Pending" : "Mark As Done"}
