cfd254c86771ceebb604b414f70126712dbc5a1d
999-4L <112986907+999-4L@users.noreply.github.com>
2024-04-09 22:54:47 +0530

    Update AppName.jsx

--- src/components/AppName.jsx
+++ src/components/AppName.jsx
@@ -1,6 +1,6 @@
 function AppName() {
   return (
-    <h1 className="font-bold text-3xl text-center text-gray-400">TaskTrack</h1>
+    <h1 className="font-bold text-3xl text-center text-gray-300">TaskTrack</h1>
   );
 }
 export default AppName;
